0f6091d2ce9e8a321847f34b82c15862696d4690
Dimpho Molepo <136012291+Dimpho-Molepo@users.noreply.github.com>
2024-06-11 15:16:08 +0200

    Fetch the data and mapped it to the DOM and styled it

--- Blog-Posts/src/App.jsx
+++ Blog-Posts/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react"
 import './App.css'
+import imgUrl from './assets/images/error-message.png'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = React.useState([])
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts") 
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch data")
+        }
+        return res.json()})
+      .then(data => setPosts(data))
+      .catch(error => {
+        console.log(error.message)
+        return setError(error.message)
+      });
+  }, [])
+
+  const postElements = posts.map(post => 
+
+    <div key={post.id} className="post-tile">
+      <h2 > <span>{post.id}</span>. {post.title}</h2>
+      <p>{post.body}</p>
+    </div>
+  )
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {error ? (
+        <img src={imgUrl} alt="404" />
+      ) : (
+        <>
+          <h1 className="Post">Posts</h1>
+          {postElements}
+        </>
+      )}
     </>
   )
 }
 
 export default App
+
+
+
+// export interface Root2 {
+//   userId: number
+//   id: number
+//   title: string
+//   body: string
+// }
